4ae2d7d700bf840df4b15330b331ca181c118ca3
ua3167 <ua3167@gmail.com>
2022-04-15 08:24:11 +0200

    working

--- app/Http/Controllers/passportAuthController.php
+++ app/Http/Controllers/passportAuthController.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Request;
 class passportAuthController extends Controller
 {
     public function registerUserExample(Request $request){
-        // $this->validate($request,[
-        //     'name'=>'required',
-        //     'email'=>'required|email|unique:users',
-        //     'password'=>'required|min:8',
-        // ]);
+        $this->validate($request,[
+            'name'=>'required',
+            'email'=>'required|email|Unique:users,email',
+            'password'=>'required|min:8',
+        ]);
         $user= User::create([
             'name' =>$request->name,
             'email'=>$request->email,
@@ -33,19 +33,24 @@ class passportAuthController extends Controller
      * login user to our application
      */
     public function loginUserExample(Request $request){
+        $this->validate($request,[
+            'email'=>'required|email|Exists:users,email',
+            'password'=>'required|min:8',
+        ]);
         $login_credentials=[
             'email'=>$request->email,
             'password'=>$request->password,
         ];
         if(auth()->attempt($login_credentials)){
             //generate the token for the user
+            $user = auth('user')->user();
             $user_login_token= auth()->user()->createToken('PassportExample@Section.io')->accessToken;
             //now return this token on success login attempt
-            return response()->json(['token' => $user_login_token], 200);
+            return response()->json(['token' => $user_login_token,'user'=>$user], 200);
         }
         else{
             //wrong login credentials, return, user not authorised to our system, return error code 401
-            return response()->json(['error' => 'UnAuthorised Access'], 401);
+            return response()->json(['error' => 'UnAuthorised Access'], 500);
         }
     }
 
